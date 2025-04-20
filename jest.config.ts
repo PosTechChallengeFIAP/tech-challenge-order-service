2466fa3b4ea262b3abe55e77d470bdf05e1aa65c
GregoriSimei <gregorisimei@gmail.com>
2025-04-19 21:23:49 -0300

    chore: adjust tests coverage

--- jest.config.ts
+++ jest.config.ts
@@ -13,4 +13,16 @@ export default {
       '^@test/(.*)$': '<rootDir>/src/tests/$1',
     },
     setupFiles: ['<rootDir>/jest.setup.ts'],
+    testPathIgnorePatterns: ['<rootDir>/src/tests/'],
+    coveragePathIgnorePatterns: ['<rootDir>/src/tests/'],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageThreshold: {
+      global: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
+    },
   };
